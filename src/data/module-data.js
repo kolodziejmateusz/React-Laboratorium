0f55b5afc77cfe8757f4fb9cd9fe5a5ce1732497
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-10-22 16:39:25 +0200

    Usunięcie folderu lab01

---
+++ src/data/module-data.js
@@ -0,0 +1,122 @@
+export const data = [
+  {
+    id: 1,
+    name: "Marcin",
+    birth: "2002-12-12",
+    eyes: "green"
+  },
+  {
+    id: 2,
+    name: "Michał",
+    birth: "1989-04-22",
+    eyes: "green"
+  },
+  {
+    id: 3,
+    name: "Michał",
+    birth: "1990-06-19",
+    eyes: "green"
+  },
+  {
+    id: 4,
+    name: "Piotr",
+    birth: "2001-02-15",
+    eyes: "brown"
+  },
+  {
+    id: 5,
+    name: "Piotr",
+    birth: "1997-03-17",
+    eyes: "gray"
+  },
+  {
+    id: 6,
+    name: "Karol",
+    birth: "1998-04-05",
+    eyes: "gray"
+  },
+  {
+    id: 7,
+    name: "Monika",
+    birth: "1985-06-07",
+    eyes: "gray"
+  },
+  {
+    id: 8,
+    name: "Maciek",
+    birth: "1976-05-08",
+    eyes: "blue"
+  },
+  {
+    id: 9,
+    name: "Krzysztof",
+    birth: "1996-08-08",
+    eyes: "gray"
+  },
+  {
+    id: 10,
+    name: "Marek",
+    birth: "2003-01-29",
+    eyes: "gray"
+  },
+  {
+    id: 11,
+    name: "Anna",
+    birth: "1980-04-24",
+    eyes: "blue"
+  },
+  {
+    id: 12,
+    name: "Marek",
+    birth: "2005-08-12",
+    eyes: "green"
+  },
+  {
+    id: 13,
+    name: "Wojciech",
+    birth: "1976-02-28",
+    eyes: "green"
+  },
+  {
+    id: 14,
+    name: "Ewa",
+    birth: "2008-01-29",
+    eyes: "brown"
+  },
+  {
+    id: 15,
+    name: "Marcin",
+    birth: "1995-01-20",
+    eyes: "gray"
+  },
+  {
+    id: 16,
+    name: "Agnieszka",
+    birth: "2001-03-02",
+    eyes: "gray"
+  },
+  {
+    id: 17,
+    name: "Marek",
+    birth: "1996-09-25",
+    eyes: "gray"
+  },
+  {
+    id: 18,
+    name: "Katarzyna",
+    birth: "1979-11-23",
+    eyes: "brown"
+  },
+  {
+    id: 19,
+    name: "Piotr",
+    birth: "1988-09-03",
+    eyes: "gray"
+  },
+  {
+    id: 20,
+    name: "Adam",
+    birth: "1976-03-05",
+    eyes: "green"
+  },
+];
