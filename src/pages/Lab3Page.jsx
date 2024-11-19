cc0555edb38f3114d377dd19a7145e86ecb51050
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-11-19 15:39:43 +0100

    LAB4 usunięto zbędne linie kodu

--- src/pages/Lab3Page.jsx
+++ src/pages/Lab3Page.jsx
@@ -1,9 +1,9 @@
-import { data } from "../data/module-data.js";
+// import { data } from "../data/module-data.js";
 import FlexContainer from "../components/FlexContainer.jsx";
 import ProfileCard from "../components/ProfileCard.jsx";
 
 function Lab3Page() {
-  return <FlexContainer element={ProfileCard} data={data} />;
+  return <FlexContainer element={ProfileCard} />;
 }
 
 export default Lab3Page;
